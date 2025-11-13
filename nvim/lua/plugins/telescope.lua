71f7afe88ee6009e0ab461e93987631072fe06fb
okabe-yuya <takamizawa46@gmail.com>
2025-11-13 23:54:30 +0900

    feat: multi select in telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,3 +1,18 @@
+local select_one_or_multi = function(prompt_bufnr)
+  local picker = require('telescope.actions.state').get_current_picker(prompt_bufnr)
+  local multi = picker:get_multi_selection()
+  if not vim.tbl_isempty(multi) then
+    require('telescope.actions').close(prompt_bufnr)
+    for _, j in pairs(multi) do
+      if j.path ~= nil then
+        vim.cmd(string.format('%s %s', 'edit', j.path))
+      end
+    end
+  else
+    require('telescope.actions').select_default(prompt_bufnr)
+  end
+end
+
 return {
   "nvim-telescope/telescope.nvim",
   dependencies = { "nvim-lua/plenary.nvim" },
@@ -12,6 +27,11 @@ return {
   },
   opts = {
     defaults = {
+      mappings = {
+        i = {
+          ['<CR>'] = select_one_or_multi,
+        }
+      },
       file_ignore_patterns = {
         "node_modules",
         "%.git/",
