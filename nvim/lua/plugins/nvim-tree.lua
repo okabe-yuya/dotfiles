20e8e2da41f60dca0860d204bbe6c08befdfed62
okabe-yuya <takamizawa46@gmail.com>
2025-12-24 00:33:29 +0900

    feat: oil の導入
    nvim-tree から oil に乗り換えた

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,44 +1,84 @@
+-- ## nvim-tree
+-- return {
+--   "nvim-tree/nvim-tree.lua",
+--   dependencies = { "nvim-tree/nvim-web-devicons" },
+--   lazy = true,
+--   keys = {
+--     { "<C-n>", "<cmd>NvimTreeToggle<CR>",   desc = "Toggle NvimTree" },
+--     { "<C-h>", "<cmd>NvimTreeFocus<CR>",    desc = "Focus NvimTree" },
+--     { "<C-f>", "<cmd>NvimTreeFindFile<CR>", desc = "Find file in NvimTree" },
+--   },
+--
+--   init = function()
+--     -- Neovimの組み込みファイルエクスプローラーを無効にする
+--     vim.g.loaded_netrw = 1
+--     vim.g.loaded_netrwPlugin = 1
+--   end,
+--
+--   config = function()
+--     require("nvim-tree").setup({
+--       sort = {
+--         sorter = "case_sensitive",
+--       },
+--       view = {
+--         width = 35,
+--         signcolumn = 'no',
+--       },
+--       renderer = {
+--         group_empty = true,
+--         highlight_git = false,
+--       },
+--       filters = {
+--         dotfiles = false,
+--         custom = {
+--           "^node_modules",
+--         },
+--       },
+--       update_focused_file = {
+--         enable = true,
+--       },
+--       git = {
+--         enable = false,
+--       },
+--     })
+--   end,
+-- }
+
+-- ## oil
 return {
-  "nvim-tree/nvim-tree.lua",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  lazy = true,
-  keys = {
-    { "<C-n>", "<cmd>NvimTreeToggle<CR>",   desc = "Toggle NvimTree" },
-    { "<C-h>", "<cmd>NvimTreeFocus<CR>",    desc = "Focus NvimTree" },
-    { "<C-f>", "<cmd>NvimTreeFindFile<CR>", desc = "Find file in NvimTree" },
+  "stevearc/oil.nvim",
+  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+  lazy = false,
+  opts = {
+    view_options = {
+      show_hidden = true,
+    },
+    preview_split = "right",
+    keymaps = {
+      -- default setting key
+      ["<CR>"] = "actions.select",
+      ["g?"] = { "actions.show_help", mode = "n" },
+
+      -- Telescope のキーと競合するため、無効にしておく
+      ["<C-p>"] = false,
+      ["gp"] = "actions.preview",
+
+      -- その他に default 設定にある項目
+      -- 現時点では使う予定がないが、拡張性のためコメントアウトしておく
+      -- ["<C-c>"] = { "actions.close", mode = "n" },
+      -- ["<C-l>"] = "actions.refresh",
+      -- ["-"] = { "actions.parent", mode = "n" },
+      -- ["_"] = { "actions.open_cwd", mode = "n" },
+      -- ["`"] = { "actions.cd", mode = "n" },
+    },
   },
-
-  init = function()
-    -- Neovimの組み込みファイルエクスプローラーを無効にする
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
-  end,
-
-  config = function()
-    require("nvim-tree").setup({
-      sort = {
-        sorter = "case_sensitive",
-      },
-      view = {
-        width = 35,
-        signcolumn = 'no',
-      },
-      renderer = {
-        group_empty = true,
-        highlight_git = false,
-      },
-      filters = {
-        dotfiles = false,
-        custom = {
-          "^node_modules",
-        },
-      },
-      update_focused_file = {
-        enable = true,
-      },
-      git = {
-        enable = false,
-      },
-    })
-  end,
+  use_default_keymaps = false,
+  keys = {
+    -- Ctrl-n で Oil 起動
+    {
+      "<C-n>",
+      "<cmd>Oil --preview<CR>",
+      desc = "Open oil explorer"
+    },
+  },
 }
