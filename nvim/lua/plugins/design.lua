77cf0e2c336d05a7891b3d3ce1710385549ca7d7
okabe-yuya <takamizawa46@gmail.com>
2025-12-25 23:03:55 +0900

    feat: dropbarのインストール

--- nvim/lua/plugins/design.lua
+++ nvim/lua/plugins/design.lua
@@ -21,6 +21,22 @@ return {
     end
   },
 
+
+  {
+    'Bekaboo/dropbar.nvim',
+    -- optional, but required for fuzzy finder support
+    dependencies = {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make'
+    },
+    config = function()
+      local dropbar_api = require('dropbar.api')
+      vim.keymap.set('n', '<Leader>l', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
+      vim.keymap.set('n', '<Leader>gg', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
+      vim.keymap.set('n', '<Leader>vn', dropbar_api.select_next_context, { desc = 'Select next context' })
+    end
+  }
+
   -- status line
   -- {
   --   'nvim-lualine/lualine.nvim',
