dd97c5041fce331cc16452c80d8392d5c93b9769
okabe-yuya <takamizawa46@gmail.com>
2026-06-29 17:07:30 +0900

    chore: コメントアウトされたnvim-tree/lualine設定を削除
    oilとデフォルトstatuslineに移行済みのため不要。

--- nvim/lua/plugins/design.lua
+++ nvim/lua/plugins/design.lua
@@ -29,34 +29,5 @@ return {
       vim.keymap.set('n', '<Leader>gg', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
       vim.keymap.set('n', '<Leader>vn', dropbar_api.select_next_context, { desc = 'Select next context' })
     end
-  }
-
-  -- status line
-  -- {
-  --   'nvim-lualine/lualine.nvim',
-  --   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  --   config = function()
-  --     -- デフォルトのステータスラインは非表示に
-  --     vim.opt.showmode = false
-  --
-  --     require('lualine').setup {
-  --       options = {
-  --         theme = 'auto',
-  --         disabled_filetypes = {
-  --           statusline = { "NvimTree" },
-  --           winbar = { 'NvimTree' },
-  --         },
-  --         globalstatus = true,
-  --       },
-  --       sections = {
-  --         lualine_c = {
-  --           { 'filename', path = 1 },
-  --         },
-  --         lualine_x = {},
-  --         lualine_y = {},
-  --         lualine_z = {},
-  --       },
-  --     }
-  --   end
-  -- },
+  },
 }
